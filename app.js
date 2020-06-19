6dddc2c07460a128e603fc873c0277391bb1191d
Aayush Chodvadiya <aayush.chodvadiya2019@vitstudent.ac.in>
2020-06-19 20:00:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ function init(){
 
     var canvas=document.getElementById("mycanvas");
 
-    W=canvas.width=600;
-    H=canvas.height=600;
+    W=canvas.width=500;
+    H=canvas.height=500;
     cs=57;
     //canvas is used to draw graphics
     pen=canvas.getContext('2d')
